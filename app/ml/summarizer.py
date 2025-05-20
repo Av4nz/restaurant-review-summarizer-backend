94ba50de8d1bd75b17e470195c902dbd3638b3df
SimuhMamad <muhammadabdurrahman891@gmail.com>
2025-05-20 14:40:13 +0700

    update summarizer.py

--- app/ml/summarizer.py
+++ app/ml/summarizer.py
@@ -13,3 +13,29 @@ def summarize_reviews(csv_path):
     inputs = tokenizer.encode("ringkaskan: " + text, return_tensors="pt", max_length=1024, truncation=True)
     summary_ids = model.generate(inputs, max_length=200, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+# from transformers import T5Tokenizer, T5ForConditionalGeneration
+# import pandas as pd
+# tokenizer = T5Tokenizer.from_pretrained("cahya/t5-base-indonesian-summarization-cased")
+# model = T5ForConditionalGeneration.from_pretrained("cahya/t5-base-indonesian-summarization-cased")
+
+# def summarize_article(csv_path):
+#     df = pd.read_csv(csv_path)
+#     reviews = df['review'].dropna().tolist()
+#     text = ' '.join(reviews)
+#     input_ids = tokenizer.encode("ringkaskan: " + text, return_tensors='pt')
+#     summary_ids = model.generate(input_ids,
+#                 min_length=50,
+#                 max_length=200,
+#                 num_beams=10,
+#                 repetition_penalty=2.5,
+#                 length_penalty=1.0,
+#                 early_stopping=True,
+#                 no_repeat_ngram_size=2,
+#                 use_cache=True,
+#                 do_sample = True,
+#                 temperature = 0.8,
+#                 top_k = 50,
+#                 top_p = 0.95)
+
+#     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
