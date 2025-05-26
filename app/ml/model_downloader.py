5646a5749fed8a442b84c34c8212004c5bbe94ca
Av4nz <cinxzaned2003@gmail.com>
2025-05-27 01:24:25 +0700

    API Finalization

--- app/ml/model_downloader.py
+++ app/ml/model_downloader.py
@@ -4,14 +4,9 @@ from pathlib import Path
 from app.core.config import MODEL_PATH
 
 def download_model_from_gdrive(dest_path):
-    url = "https://drive.google.com/file/d/16W45YwmyasFFcUE05t-z1v3Y0iJMdjG3/view?usp=sharing"
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(dest_path, "wb") as f:
-            for chunk in response.iter_content(1024):
-                f.write(chunk)
-    else:
-        raise Exception("Failed to download model from Google Drive")
+    import gdown
+    url = "https://drive.google.com/uc?id=16W45YwmyasFFcUE05t-z1v3Y0iJMdjG3"
+    gdown.download(url, str(dest_path), quiet=False)
     
 def ensure_model_downloaded():
     """Ensure the model is downloaded from Google Drive."""
